1c63591af0315306901da278b2efdbfdb2f832a2
Вергазов Артём <vergazka@gmail.com>
2024-08-13 15:31:15 +0300

    перевод на yii2

--- dump.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO categories(name,icon) VALUES('Курьерские', 'courier');
--- helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function dump($data): void
-{
-    echo '<pre>';
-    print_r($data);
-    echo '</pre>';
-}
