d29758e43bf661a3513b742266eb0d023a06974b
Я <vergazka@gmail.com>
2024-08-26 22:52:23 +0300

    Добавил часть миграций

---
+++ migrations/m240826_164727_create_user_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m240826_164727_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'email' => $this->string(),
+            'password' => $this->string(),
+            'city_id' => $this->integer(),
+            'role' => $this->string(),
+            'birthdate' => $this->date(),
+            'phone' => $this->string(),
+            'telegram' => $this->string(),
+            'info' => $this->text(),
+            'avatar' => $this->string(),
+            'failed_tasks' => $this->integer(),
+        ]);
+
+        $this->addForeignKey(
+            'fk-user-city_id',
+            'user',
+            'city_id',
+            'city',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
---
+++ migrations/m240826_183015_create_task_table.php
@@ -0,0 +1,73 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%task}}`.
+ */
+class m240826_183015_create_task_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%task}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(50)->notNull(),
+            'description' => $this->text()->notNull(),
+            'category_id' => $this->integer()->notNull(),
+            'city_id' => $this->integer()->notNull(),
+            'coordinates' => $this->string(100),
+            'budget' => $this->integer(),
+            'deadline' => $this->date(),
+            'author_id' => $this->integer()->notNull(),
+            'performer_id' => $this->integer()->notNull(),
+            'status' => $this->string(50)->notNull(),
+        ]);
+
+        $this->addForeignKey(
+            'fk-task-category_id',
+            'task',
+            'category_id',
+            'category',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-task-city_id',
+            'task',
+            'city_id',
+            'city',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-task-author_id',
+            'task',
+            'author_id',
+            'user',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-task-performer_id',
+            'task',
+            'performer_id',
+            'user',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%task}}');
+    }
+}
