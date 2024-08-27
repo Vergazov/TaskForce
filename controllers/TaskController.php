664297937e5c5bd0143299d2727339939836cb38
Вергазов Артём <vergazka@gmail.com>
2024-08-27 15:40:14 +0300

    Добавил контроллер для задач и вью. Сделал начальную верстку для отображения задач

---
+++ controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Task;
+use yii\web\Controller;
+
+class TaskController extends Controller
+{
+    public function actionIndex()
+    {
+        $tasks = Task::find()
+            ->where(['status' => '1'])
+            ->with('category', 'city', 'author', 'performer')
+            ->all();
+
+        return $this->render('index', ['tasks' => $tasks]);
+    }
+
+}
